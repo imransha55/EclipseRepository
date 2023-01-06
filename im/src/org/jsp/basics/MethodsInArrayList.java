092115376f994c03d99eda6eaf1abdda19b9b2c3
imransha55 <118876326+imransha55@users.noreply.github.com>
2023-01-06 11:14:41 +0530

    Add files via upload

---
+++ im/src/org/jsp/basics/MethodsInArrayList.java
@@ -0,0 +1,62 @@
+package org.jsp.basics;
+
+public class MethodsInArrayList {
+
+	public static void main(String[] args) {
+		System.out.println("add()"
+				+ "inserts the element to the arraylist\n");
+		System.out.println("addAll()\n"
+				+ "adds all elements of a collection to arraylist\n");
+		System.out.println("clear()\n"
+				+ "removes all the elements from arraylist\n");
+		System.out.println("clone()\n"
+				+ "makes a copy of the array list\n");
+		System.out.println("contains()\n"
+				+ "checks if the element is present in the arraylist\n");
+		System.out.println("get()\n"
+				+ "returns the element present in the specified index\n");
+		System.out.println("indexOf()\n"
+				+ "returns the position of the specified element\n");
+		System.out.println("removeAll()\n"
+				+ "removes multiple elements from the arraylist\n");
+		System.out.println("remove()\n"
+				+ "removes the single element from the arraylist\n");
+		System.out.println("size()\n"
+				+ "returns the length of an arraylist\n");
+		System.out.println("isEmpty()\n"
+				+ "checks if the arraylist is empty\n");
+		System.out.println("subList()\n"
+				+ "returns a portion of the arraylist\n");
+		System.out.println("set()\n"
+				+ "replace the single element from an arraylist\n");
+		System.out.println("sort()\n"
+				+ "sorts the arraylist according to specified order\n");
+		System.out.println("toArray()\n"
+				+ "converts an arraylist to an array\n");
+		System.out.println("toString()\n"
+				+ "converts the arraylist into a String\n");
+		System.out.println("ensureCapacity()\n"
+				+ "set the size of an araylist\n");
+		System.out.println("lastIndexOf()\n"
+				+ "returns position of last occurrence of the element\n");
+		System.out.println("retainAll()\n"
+				+ "retains only the common elements\n");
+		System.out.println("containsAll()\n"
+				+ "checks if a collection is a subset of arraylist\n");
+		System.out.println("trimToSize()\n"
+				+ "trims the capacity of arraylist equal to the size\n");
+		System.out.println("removeRange()\n"
+				+ "removes a portion of the arraylist\n");
+		System.out.println("replaceAll()\n"
+				+ "replace all elements from the arraylist\n");
+		System.out.println("removeIf()\n"
+				+ "removes element that satisfy the condition\n");
+		System.out.println("forEach()\n"
+				+ "performs an action to all elements of arraylist\n");
+		System.out.println(" iterator()\n"
+				+ "returns an iterate to loop through the ArrayList\n");
+		
+
+	}
+
+}
